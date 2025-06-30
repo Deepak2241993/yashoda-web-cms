09b1763ddd93594be481a7ed13f05ce00fd6efe5
sak03 <sartaj2394@gmail.com>
2025-06-30 11:14:32 +0530

    files removed

--- src/redux/actions/astroViewModeAction.js
+++
@@ -1,8 +0,0 @@
-import {ActionTypes} from '../typeConstants/typeConstants'
-
-export const setAstroViewMode = (value)=>{
-    return{
-        type:ActionTypes.SET_ASTRO_VIEW_MODE,
-        viewMode:value
-    }
-}
